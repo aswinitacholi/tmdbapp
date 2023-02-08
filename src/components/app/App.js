12b44d2e08050c04221ce015f11b7f53fe999f49
unknown <pwcreact19@iiht.tech>
2023-02-08 10:36:56 +0530

    intial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+
+  useEffect(() =>{
+    console.log("mount");
+  },[]);
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
